2de877ce8002647e51c5b46cf28f1a396d55f6b8
Thiago Santos <thia.mdossantos@gmail.com>
2018-10-25 08:42:57 -0300

    feat: add coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Srp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.travis": :test, "coveralls.html": :test]
     ]
   end
 
@@ -21,7 +23,8 @@ defmodule Srp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10.1", only: :test}
     ]
   end
 end
