818a5ed5d93b233713279c88aca54cb99084347a
Thiago Santos <thia.mdossantos@gmail.com>
2018-11-02 21:51:27 -0300

    docs: add login usage

--- lib/srp.ex
+++ lib/srp.ex
@@ -34,6 +34,63 @@ defmodule SRP do
   # Server stores the information
   ```
 
+  ## Logging in
+
+  Authenticating a user on the server involves multiple steps.
+
+  1. The client sends to the server the username.
+  2. The server finds the password verifier and salt for that username.
+  Then it generates a ephemeral key pair and sends back to the client the salt and the public key.
+
+  ```elixir
+  # Find the record for the given username
+  # Load from the database the password_verifier, and the salt
+  key_pair = SRP.server_key_pair(password_verifier)
+
+  # Send back to the client -> key_pair.public + salt
+  ```
+
+  If the username does not exist the server can send a fake value.
+  It is important to not reveal if an username is registered on the system or not.
+  An attacker could use the login to find the registered usernames
+  and try a dictionary attack specify for those users.
+
+  3. The client generates a key pair and a client proof of identity.
+  Then the client sends to the server the proof and the client's public key.
+
+  ```elixir
+  # receives from the server the server_public_key and the salt.
+
+  identity = SRP.Identity.new(username, password)
+  key_pair = SRP.client_key_pair()
+  proof = SRP.client_proof(identity, salt, key_pair, server_public_key)
+
+  # Send to the server -> proof + server_public_key
+  ```
+
+  4. Server verify client proof then build its own proof of identity.
+  Then sends back the server's proof.
+
+  ```elixir
+  valid? = SRP.valid_client_proof?(client_proof, password_verifier, server_key_pair, client_public_key)
+
+  if valid? do
+    # Send back to client the server's proof -> server_proof
+  else
+    # Send back unauthorized
+  end
+  ```
+
+  5. The client receives the server's proof and validates it.
+  This step can be skipped if you don't feel the need to verify the server's identity.
+
+  ```elixir
+  identity = SRP.Identity.new(username, password)
+  valid? = SRP.valid_server_proof?(server_proof, identity, salt, client_key_pair, server_public_key)
+  ```
+
+  From now on is to safe to create a new session between the client and server.
+
   ## Prime Groups
 
   The default prime size is 2048. Each prime group contains a large prime and a generator.
